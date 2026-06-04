df4da9761635a988b12c9048d68bcb18d709fb62
klen.copic.pucihar <klen.copic@famnit.upr.si>
2026-06-04 07:37:44 +0200

    big truck

---
+++ Assets/Big Truck/Scripts/rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.localPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z+rotationSpeed);
+        transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime);
+    }
+}
