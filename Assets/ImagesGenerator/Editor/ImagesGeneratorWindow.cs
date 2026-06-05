68f0d8f7adb61a95e2ded431e649f3c0d3488522
Marjan Meglen <marjanmeglen@gmail.com>
2026-06-05 14:32:23 +0200

    Add configurable truck placement range

--- Assets/ImagesGenerator/Editor/ImagesGeneratorWindow.cs
+++ Assets/ImagesGenerator/Editor/ImagesGeneratorWindow.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 
 public class ImagesGeneratorWindow : EditorWindow
 {
+    [SerializeField] private Camera captureCamera;
+    [SerializeField] private GameObject truck;
+    [SerializeField] private Vector3 startPosition;
+    [SerializeField] private Vector3 endPosition;
+    [SerializeField] private int imageCount = 10;
+
+    private bool hasStartPosition;
+    private bool hasEndPosition;
+
     [MenuItem("Tools/Images Generator")]
     public static void Open()
     {
@@ -13,7 +22,64 @@ public class ImagesGeneratorWindow : EditorWindow
     {
         EditorGUILayout.LabelField("Images Generator", EditorStyles.boldLabel);
         EditorGUILayout.HelpBox(
-            "Image generation settings and controls will be added here.",
+            "Configure the camera, truck, and movement range for image generation.",
             MessageType.Info);
+
+        captureCamera = (Camera)EditorGUILayout.ObjectField("Camera", captureCamera, typeof(Camera), true);
+        truck = (GameObject)EditorGUILayout.ObjectField("Truck", truck, typeof(GameObject), true);
+
+        EditorGUILayout.Space();
+        DrawPositionRangeFields();
+
+        EditorGUILayout.Space();
+        imageCount = EditorGUILayout.IntField("Number Of Images", imageCount);
+    }
+
+    private void DrawPositionRangeFields()
+    {
+        EditorGUILayout.LabelField("Truck Position Range", EditorStyles.boldLabel);
+
+        using (new EditorGUI.DisabledScope(truck == null))
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUI.BeginChangeCheck();
+            startPosition = EditorGUILayout.Vector3Field("Start Position", startPosition);
+            if (EditorGUI.EndChangeCheck())
+            {
+                hasStartPosition = true;
+            }
+            if (GUILayout.Button("Use Truck Position", GUILayout.Width(125)))
+            {
+                startPosition = truck.transform.position;
+                hasStartPosition = true;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUI.BeginChangeCheck();
+            endPosition = EditorGUILayout.Vector3Field("End Position", endPosition);
+            if (EditorGUI.EndChangeCheck())
+            {
+                hasEndPosition = true;
+            }
+            if (GUILayout.Button("Use Truck Position", GUILayout.Width(125)))
+            {
+                endPosition = truck.transform.position;
+                hasEndPosition = true;
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (truck == null)
+        {
+            EditorGUILayout.HelpBox("Select a truck before capturing start and end positions.", MessageType.Warning);
+        }
+        else if (!hasStartPosition || !hasEndPosition)
+        {
+            EditorGUILayout.HelpBox(
+                "Move the truck to each endpoint and click Use Truck Position. " +
+                "You can also type the coordinates manually.",
+                MessageType.Warning);
+        }
     }
 }
